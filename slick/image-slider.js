590dafa91f1689a5a82f7d4ac29ee68feabbe784
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-08-30 20:50:26 -0700

    initial commit

---
+++ slick/image-slider.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){ 
+	$('.slider-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: true,
+	  fade: true,
+	  asNavFor: '.slider-nav'
+	});
+	$('.slider-nav').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider-for',
+	  dots: true,
+	  focusOnSelect: true,
+		infinite: true,
+		centerMode: true,
+		arrows: false,
+		variableWidth: true
+	});
+});
